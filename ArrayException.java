f244494fcb0fe40a341d1dd1e7f4fe9f0a761b71
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-14 11:17:22 +0530

    Add files via upload

--- ArrayException.java
+++ ArrayException.java
@@ -4,11 +4,13 @@ public static void main(String[] args)
 {
 	String []names= {"Madhushree","Prateeka","Raksha"};
 	for(int i=0;i<names.length;i++)
-	{
+	{ 
+		try{
 		System.out.println(names[i]);
-		
 	}
-	System.out.println("End of Main()");	
-	
+	catch(ArrayIndexOutOfBoundsException e){
+	System.out.println("e.getMessage()");	
+	}
+}
 }
 }
